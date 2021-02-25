a88ee1c71aebdc8b64ccf8ee069568cba74bd753
Dmitriy <dim.fly.design@gmail.com>
2021-02-25 18:21:06 +0600

    back

--- packages/aws-appsync-subscription-link/src/realtime-subscription-handshake-link.ts
+++ packages/aws-appsync-subscription-link/src/realtime-subscription-handshake-link.ts
@@ -559,16 +559,7 @@ export class AppSyncRealTimeSubscriptionHandshakeLink extends ApolloLink {
           };
           this.awsRealTimeSocket.onclose = event => {
             logger(`WebSocket closed ${event.reason}`);
-            // Error code 1000 means that the connection was closed normally.
-            if (event.code === 1000 || navigator.onLine) {
-              rej(new Error(JSON.stringify(event)));
-            } else {
-              logger("You are offline. Reconnecting once online");
-              // Initialize handshake once online again
-              window.addEventListener('online', () => {
-                this._initializeHandshake({ awsRealTimeUrl });
-              });
-            }
+            rej(new Error(JSON.stringify(event)));
           };
 
           this.awsRealTimeSocket.onmessage = (message: MessageEvent) => {
